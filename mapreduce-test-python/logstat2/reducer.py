cc8c4f136ef61c748f8a71b3eb0e81634b2d76b4
Weiaozhang <37387679+Weiaozhang@users.noreply.github.com>
2021-03-15 21:30:23 -0400

    Update reducer.py

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -14,7 +14,7 @@ for line in sys.stdin:
     except ValueError:
         pass
 
-
+#test edit
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
 for ip, count in sorted_dict_ip_count:
     print '%s\t%s' % (ip, count)

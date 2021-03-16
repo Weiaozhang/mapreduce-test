5ca7bc3cc5807bacab10dc8462e6387a493e399d
Weiaozhang <37387679+Weiaozhang@users.noreply.github.com>
2021-03-16 15:41:59 -0400

    Update reducer.py

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -19,13 +19,13 @@ for line in sys.stdin:
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
 
 #test edit
-all_values = [x[0][1:3] for x in list_of_lists]
+all_values = [x[0][1:3] for x in sorted_dict_ip_count]
 unique_values = set(all_values)
 
 group_list = []
 for value in unique_values:
   this_group = []
-  for x in list_of_lists:
+  for x in sorted_dict_ip_count:
     if x[0][1:3] == value:
       this_group.append(x)
   group_list.append(this_group)
